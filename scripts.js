1db59ecf48b96ee8719a34e22baed16713613710
mrKorg <student030306>
2018-01-10 23:52:29 +0200

    'fix'

--- scripts.js
+++ scripts.js
@@ -64,6 +64,7 @@ window.onload = function () {
         for (let i = 0; i < carts.length; i++) {
             if (carts.hasOwnProperty(i)) {
                 carts[i].onclick = function () {
+                    if (document.getElementsByClassName('show').length === 2) return;
                     if (data.selected.item_1.str === null || data.selected.item_2.str === null) {
                         let self = this;
                         self.classList.add('show');
@@ -91,7 +92,11 @@ window.onload = function () {
                         }
                         if (data.selected.item_1.str !== null && data.selected.item_2.str !== null && data.selected.item_1.str === data.selected.item_2.str) {
                             moveSelected(data.selected.item_1.id, data.selected.item_2.id);
-                            data.selected.item_1 = data.selected.item_2 = {
+                            data.selected.item_1 = {
+                                id: null,
+                                str: null
+                            };
+                            data.selected.item_2 = {
                                 id: null,
                                 str: null
                             };
@@ -103,17 +108,19 @@ window.onload = function () {
     }
 
     function moveSelected(item_id_1, item_id_2) {
-        data.identified.push(data.imagesArray[item_id_1]);
-        let cart1 = document.getElementById('cart-' + item_id_1);
-        let cart2 = document.getElementById('cart-' + item_id_2);
-        cart1.parentNode.removeChild(cart1);
-        cart2.parentNode.removeChild(cart2);
-        let newCart = document.createElement('p');
-        newCart.className = "cart";
-        newCart.innerHTML = cart1.innerHTML;
-        resultBox.appendChild(newCart);
-        if (data.identified.length === data.items.length) {
-            alert('You win!!!')
+        if (item_id_1 !== null && item_id_2 !== null) {
+            data.identified.push(data.imagesArray[item_id_1]);
+            let cart1 = document.getElementById('cart-' + item_id_1);
+            let cart2 = document.getElementById('cart-' + item_id_2);
+            cart1.parentNode.removeChild(cart1);
+            cart2.parentNode.removeChild(cart2);
+            let newCart = document.createElement('p');
+            newCart.className = "cart";
+            newCart.innerHTML = cart1.innerHTML;
+            resultBox.appendChild(newCart);
+            if (data.identified.length === data.items.length) {
+                alert('You win!!!')
+            }
         }
     }
 
